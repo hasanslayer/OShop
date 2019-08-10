ff312f122730746758a0da14ad6a702b09da29e9
hasanslayer <hasan-dr2010@hotmail.com>
2019-08-11 00:55:52 +0200

    Add bootstrap card

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../product.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  products$;
 
-  constructor() { }
+  constructor(private productService:ProductService) {
+    this.products$ = this.productService.getAll();
+  }
 
   ngOnInit() {
   }
+
 
 }
--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,15 +11,18 @@ import { ProductService } from '../../product.service';
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService:CategoryService,private productService:ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private productService: ProductService
+  ) {
     this.categories$ = categoryService.getCategories();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  save(product){
+  save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products'])
   }
-
 }
