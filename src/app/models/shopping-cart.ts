f889f651c3c46e9689af3e7890de1397205048b5
hasanslayer <hasan-dr2010@hotmail.com>
2019-08-16 23:27:18 +0200

    Display total price.

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -3,8 +3,10 @@ import { ShoppingCartItem } from './shopping-cart-item';
 export class ShoppingCart {
   items: ShoppingCartItem[] = [];
   constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
-    for (let productId in itemsMap)
-      this.items.push(itemsMap[productId]);
+    for (let productId in itemsMap) {
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+    }
   }
 
   get totalItemsCount() {
@@ -14,5 +16,13 @@ export class ShoppingCart {
     }
 
     return count;
+  }
+
+  get totalPrice() {
+    let sum = 0;
+    for (let productId in this.items) {
+      sum += this.items[productId].totalPrice;
+    }
+    return sum;
   }
 }
