89b8d318a43f76c48eff5307da2975aca8e8c22c
hasanslayer <hasan-dr2010@hotmail.com>
2019-08-17 06:30:54 +0200

    Simplify shopping cart object

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -4,9 +4,14 @@ import { Product } from './product';
 export class ShoppingCart {
   items: ShoppingCartItem[] = [];
   constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+    this.itemsMap = itemsMap || {};
+
     for (let productId in itemsMap) {
       let item = itemsMap[productId];
-      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+      let x = new ShoppingCartItem();
+      Object.assign(x, item);
+      x.$key = productId;
+      this.items.push(x);
     }
   }
 
@@ -28,7 +33,6 @@ export class ShoppingCart {
   }
 
   getQuantity(product: Product) {
-    console.log('product',product)
     let item = this.itemsMap[product.$key];
     return item ? item.quantity : 0;
   }
