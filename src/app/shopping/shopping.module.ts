5de68843857d0406dc37f80950a3dfb9880434f1
hasanslayer <hasan-dr2010@hotmail.com>
2019-08-19 20:03:17 +0200

    Creating shopping modul.

---
+++ src/app/shopping/shopping.module.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthGuardService as AuthGuard } from 'shared/services/auth-guard.service';
+import { SharedModule } from 'shared/shared.module';
+
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { OrderDetailComponent } from './components/order-detail/order-detail.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
+import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ShoppingCartComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+
+      {
+        path: 'check-out',
+        component: CheckOutComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'order-success/:id',
+        component: OrderSuccessComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'order-detail/:id',
+        component: OrderDetailComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'my/orders',
+        component: MyOrdersComponent,
+        canActivate: [AuthGuard]
+      },
+    ])
+  ],
+  declarations: [
+    ShoppingCartComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    ProductFilterComponent,
+    ShoppingCartSummaryComponent,
+    ShippingFormComponent,
+    OrderDetailComponent
+  ]
+})
+export class ShoppingModule { }
